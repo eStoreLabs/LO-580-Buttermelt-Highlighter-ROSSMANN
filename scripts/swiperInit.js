7d38d936f40d95ee14febad6b0137e79689d2fa3
WitchDevelops <d.n.wojewska@gmail.com>
2025-05-25 20:56:10 +0200

    add swiper

---
+++ scripts/swiperInit.js
@@ -0,0 +1,22 @@
+(() => {
+  const swiper = new Swiper("#esColorsSwiper", {
+    enabled: false, // start disabled for mobile
+    slidesPerView: 1,
+    loop: true,
+    authoheight: true,
+    autoplay: true,
+    pagination: {
+      el: "#es-swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: "#es-button-next",
+      prevEl: "#es-button-prev",
+    },
+    breakpoints: {
+      992: {
+        enabled: true,
+      },
+    },
+  });
+})();
