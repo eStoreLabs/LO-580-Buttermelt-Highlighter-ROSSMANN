ad5519741cd51a6673445bc00b5507271324f0ef
WitchDevelops <d.n.wojewska@gmail.com>
2025-05-28 22:03:59 +0200

    fix: run swiper script only when it's not initialized

--- scripts/swiperInit.js
+++ scripts/swiperInit.js
@@ -1,22 +1,24 @@
 (() => {
-  const swiper = new Swiper("#esColorsSwiper", {
-    enabled: false, // start disabled for mobile
-    slidesPerView: 1,
-    loop: true,
-    authoHeight: true,
-    autoplay: true,
-    pagination: {
-      el: "#es-swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: "#es-button-next",
-      prevEl: "#es-button-prev",
-    },
-    breakpoints: {
-      992: {
-        enabled: true,
+  if (!document.querySelector("#esColorsSwiper.swiper-initialized")) {
+    const swiper = new Swiper("#esColorsSwiper", {
+      enabled: false, // start disabled for mobile
+      slidesPerView: 1,
+      loop: true,
+      authoHeight: true,
+      autoplay: true,
+      pagination: {
+        el: "#es-swiper-pagination",
+        clickable: true,
       },
-    },
-  });
+      navigation: {
+        nextEl: "#es-button-next",
+        prevEl: "#es-button-prev",
+      },
+      breakpoints: {
+        992: {
+          enabled: true,
+        },
+      },
+    });
+  }
 })();
